d3b3079eff154943b71a17f5c442b3d3edd92443
RehanaKauser <120611543+RehanaKauser@users.noreply.github.com>
2024-03-15 12:00:25 +0000

    final

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 
-// Write a function that returns an object of array containing the names of the months of the year that have 31 days. 
+// Define an array of month objects, each containing the name of the month and its corresponding number of days.
+// Filter out the months with 31 days from the array.
 
 // Define a function to find the months that have 31 days in them.
 export const findTheDaysInMonth = (daysInMonth) => {
