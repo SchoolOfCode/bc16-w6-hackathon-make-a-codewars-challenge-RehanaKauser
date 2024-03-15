eaad8881f900ef72e0bc04c13e002d7bdda3f738
RehanaKauser <120611543+RehanaKauser@users.noreply.github.com>
2024-03-15 12:21:31 +0000

    added solution.js file

---
+++ solution.js
@@ -0,0 +1,11 @@
+// Define a function to find the months that have 31 days in them.
+export const findTheDaysInMonth = (daysInMonth) => {
+
+    // write code here
+
+    const expected = [{month: "January", days: 31}, {month: "March", days: 31}, {month: "May", days: 31}, {month: "July", days: 31},
+ {month: "August", days: 31}, {month: "October", days: 31}, {month: "December", days: 31} ];
+
+    return expected
+
+};
